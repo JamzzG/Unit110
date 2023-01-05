460f116a642f3f17eff9cc92b7fb10478f3bcd5a
JamzzG <granthamjames@yahoo.com>
2023-01-04 20:47:53 -0700

    Class 4 changes for 108 project

--- server.py
+++ server.py
@@ -33,6 +33,68 @@ def version():
 def get_catalog():
     return json.dumps(catalog)
 
+
+@app.get("/api/catalog/<category>")
+def get_by_category(category):
+    result = []
+    for prod in catalog:
+        if prod["category".lower()] == category.lower():
+            result.append(prod)
+
+    return json.dumps(result)
+
+
+@app.get("/api/catalog/search/<title>")
+def search_by_title(title):
+    result = []
+    for item in catalog:
+        if title.lower() in item["title"].lower():
+            result.append(item)
+    
+    return json.dumps(result)
+    
+@app.get("/api/product/cheaper/<limit>")
+def cheaper_than(limit):
+    result = []
+    for prod in catalog:
+        if prod["price"]< float(limit):
+            result.append(prod)
+
+    return json.dumps(result)
+
+
+@app.get("/api/product/total_items")
+def total_items():
+    count = len(catalog)
+    return json.dumps(count)
+#     json.dumps 
+
+
+# The code below was modified from CHAT GPT
+@app.get("/api/product/lowest")
+def find_lowest_price():
+    lowest_price = float('inf')
+    lowest_priced_item = None
+    for item in catalog:
+        if item['price'] < lowest_price:
+            lowest_priced_item = item
+            lowest_price = item['price']
+    return json.dumps(lowest_priced_item)
+    
+@app.get("/api/product/cheapest")
+def get_cheapest():
+    answer = catalog [0]
+    for prod in catalog:
+        if prod["price"] < answer["price"]:
+            answer = prod 
+    return json.dumps(answer)
+
+
+
+
+
+
+
 @app.get("/test/numbers")
 def get_numbers():
     # create a list with numbers from 1 to 20
